10e6fc814e48d8aa3126539c1fa724fc0cbe833d
guilherme <ggolopes@gmail.com>
2023-11-01 22:09:29 -0400

    Logic finished

--- FormReports.cs
+++ FormReports.cs
@@ -13,7 +13,7 @@ namespace PHD_ChallengeDesktop
 {
     public partial class FormReports : Form
     {
-        SqlConnection DB_conn = new SqlConnection(@"Server=NBASUSGUIGA;Database=phdChallenge_DB;Trusted_Connection=True;MultipleActiveResultSets=true;TrustServerCertificate=true");
+        SqlConnection DB_conn = new SqlConnection(@"Server=NBASUSGUIGA;Database=phdChallenge_DB;Trusted_Connection=True;User ID=phd;Password=phdsolutions;MultipleActiveResultSets=true;TrustServerCertificate=true");
         SqlCommand sqlCmd;
         SqlDataAdapter sqlDataAdapter;
         DataTable dataTable;
@@ -29,106 +29,103 @@ namespace PHD_ChallengeDesktop
 
         private void ShowProjects()
         {
-            using (SqlConnection conn = DB_conn)
+            SqlConnection conn = DB_conn;
+            try
             {
-                try
-                {
-                    conn.Open();
-                    sql = " SELECT pr.projet AS Proj_Number, pj.ProjectDescription AS Project_Name," +
-                            " ROUND(sum((pr.costPer*pr.quantity)),2) AS Project_Cost_Total" +
-                            " FROM PurchaseRecord pr JOIN ProjectRecord pj ON pj.projectNumber = pr.projet" +
-                            " GROUP BY pr.projet, pj.projectDescription";
-                    sqlCmd = new SqlCommand(sql, conn);
-                    SqlDataReader reader = sqlCmd.ExecuteReader();
-                    dataTable = new DataTable();
-                    dataTable.Load(reader);
-                    reader.Close();
-                    dbgvProjects.DataSource = dataTable;
-                }
-                catch (SqlException ex)
-                {
-                    MessageBox.Show(ex.Message);
-                }
-                finally
-                {
-                    conn.Close();
-                }
+
+                conn.Open();
+                sql = " SELECT pr.projet AS Proj_Number, pj.ProjectDescription AS Project_Name," +
+                        " ROUND(sum((pr.costPer*pr.quantity)),2) AS Project_Cost_Total" +
+                        " FROM PurchaseRecord pr JOIN ProjectRecord pj ON pj.projectNumber = pr.projet" +
+                        " GROUP BY pr.projet, pj.projectDescription";
+                sqlCmd = new SqlCommand(sql, conn);
+                SqlDataReader reader = sqlCmd.ExecuteReader();
+                dataTable = new DataTable();
+                dataTable.Load(reader);
+                reader.Close();
+                dbgvProjects.DataSource = dataTable;
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
             }
         }
 
         public void ShowPurchaseOrdersByProject(string projNumber)
         {
-            using (SqlConnection conn = DB_conn)
+            SqlConnection conn = DB_conn;
+            try
             {
-                try
-                {
-                    //Purchases
-                    conn.Open();
-                    sql = " SELECT pj.projectNumber, pj.projectDescription AS Proj_Name, u.fullName AS Purchaser_Name," +
-                          " pr.partNumber, mm.Description, pr.quantity, pr.costPer" +
-                          " FROM ProjectRecord pj JOIN PurchaseRecord pr ON pr.projet = pj.projectNumber" +
-                          " JOIN Users u ON u.userName = pr.purchaser" +
-                          " JOIN MaterialMaster mm ON pr.partNumber = mm.PartNumber" +
-                          " WHERE pj.projectNumber = @p_projectNumber;";
-                    sqlCmd = new SqlCommand(sql, conn);
-                    sqlCmd.Parameters.Add(new SqlParameter("@p_projectNumber", projNumber));
-                    SqlDataReader reader = sqlCmd.ExecuteReader();
-                    dataTable = new DataTable();
-                    dataTable.Load(reader);
-                    reader.Close();
-                    dbgvPurchases.DataSource = dataTable;
+                //Purchases
+                conn.Open();
+                sql = " SELECT pj.projectNumber, pj.projectDescription AS Proj_Name, u.fullName AS Purchaser_Name," +
+                        " pr.partNumber, mm.Description, pr.quantity, pr.costPer" +
+                        " FROM ProjectRecord pj JOIN PurchaseRecord pr ON pr.projet = pj.projectNumber" +
+                        " JOIN Users u ON u.userName = pr.purchaser" +
+                        " JOIN MaterialMaster mm ON pr.partNumber = mm.PartNumber" +
+                        " WHERE pj.projectNumber = @p_projectNumber;";
+                sqlCmd = new SqlCommand(sql, conn);
+                sqlCmd.Parameters.Add(new SqlParameter("@p_projectNumber", projNumber));
+                SqlDataReader reader = sqlCmd.ExecuteReader();
+                dataTable = new DataTable();
+                dataTable.Load(reader);
+                reader.Close();
+                dbgvPurchases.DataSource = dataTable;
 
-                    // Users
-                    sql = " SELECT DISTINCT u.fullName AS Purchaser_Fullname, count(pr.partNumber) AS Number_of_Purchases" +
-                          " FROM Users u JOIN PurchaseRecord pr ON u.userName = pr.purchaser" +
-                          " WHERE pr.projet = @p_projectNumber " +
-                          " GROUP BY u.fullName";
-                    sqlCmd = new SqlCommand(sql, conn);
-                    sqlCmd.Parameters.Add(new SqlParameter("@p_projectNumber", projNumber));
-                    reader = sqlCmd.ExecuteReader();
-                    dataTable = new DataTable();
-                    dataTable.Load(reader);
-                    reader.Close();
-                    dbgvUsers.DataSource = dataTable;
+                // Users
+                sql = " SELECT DISTINCT u.fullName AS Purchaser_Fullname, count(pr.partNumber) AS Number_of_Purchases" +
+                        " FROM Users u JOIN PurchaseRecord pr ON u.userName = pr.purchaser" +
+                        " WHERE pr.projet = @p_projectNumber " +
+                        " GROUP BY u.fullName";
+                sqlCmd = new SqlCommand(sql, conn);
+                sqlCmd.Parameters.Add(new SqlParameter("@p_projectNumber", projNumber));
+                reader = sqlCmd.ExecuteReader();
+                dataTable = new DataTable();
+                dataTable.Load(reader);
+                reader.Close();
+                dbgvUsers.DataSource = dataTable;
 
-                    // Parts
-                    sql = " SELECT pr.partNumber, mm.Description, SUM(pr.quantity) AS Quantity_Total," +
-                          " ROUND(SUM((pr.costPer*pr.quantity)),2) AS Cost_Total" +
-                          " FROM PurchaseRecord pr JOIN MaterialMaster mm ON pr.partNumber = mm.PartNumber" +
-                          " WHERE pr.projet = @p_projectNumber" +
-                          " GROUP BY pr.partNumber, mm.Description" +
-                          " ORDER BY pr.partNumber";
-                    sqlCmd = new SqlCommand(sql, conn);
-                    sqlCmd.Parameters.Add(new SqlParameter("@p_projectNumber", projNumber));
-                    reader = sqlCmd.ExecuteReader();
-                    dataTable = new DataTable();
-                    dataTable.Load(reader);
-                    reader.Close();
-                    dbgvParts.DataSource = dataTable;
+                // Parts
+                sql = " SELECT pr.partNumber, mm.Description, SUM(pr.quantity) AS Quantity_Total," +
+                        " ROUND(SUM((pr.costPer*pr.quantity)),2) AS Cost_Total" +
+                        " FROM PurchaseRecord pr JOIN MaterialMaster mm ON pr.partNumber = mm.PartNumber" +
+                        " WHERE pr.projet = @p_projectNumber" +
+                        " GROUP BY pr.partNumber, mm.Description" +
+                        " ORDER BY pr.partNumber";
+                sqlCmd = new SqlCommand(sql, conn);
+                sqlCmd.Parameters.Add(new SqlParameter("@p_projectNumber", projNumber));
+                reader = sqlCmd.ExecuteReader();
+                dataTable = new DataTable();
+                dataTable.Load(reader);
+                reader.Close();
+                dbgvParts.DataSource = dataTable;
 
-                    // Total Cost
-                    sql = " SELECT pr.projet AS Proj_Number, pj.ProjectDescription AS Project_Name," +
-                          " ROUND(SUM((pr.costPer*pr.quantity)),2) AS Project_Cost_Total" +
-                          " FROM PurchaseRecord pr JOIN ProjectRecord pj ON pj.projectNumber = pr.projet" +
-                          " WHERE pr.projet = @p_projectNumber" +
-                          " GROUP BY pr.projet, pj.projectDescription";
-                    sqlCmd = new SqlCommand(sql, conn);
-                    sqlCmd.Parameters.Add(new SqlParameter("@p_projectNumber", projNumber));
-                    reader = sqlCmd.ExecuteReader();
-                    dataTable = new DataTable();
-                    dataTable.Load(reader);
-                    reader.Close();
-                    dbgvTotal.DataSource = dataTable;
+                // Total Cost
+                sql = " SELECT pr.projet AS Proj_Number, pj.ProjectDescription AS Project_Name," +
+                        " ROUND(SUM((pr.costPer*pr.quantity)),2) AS Project_Cost_Total" +
+                        " FROM PurchaseRecord pr JOIN ProjectRecord pj ON pj.projectNumber = pr.projet" +
+                        " WHERE pr.projet = @p_projectNumber" +
+                        " GROUP BY pr.projet, pj.projectDescription";
+                sqlCmd = new SqlCommand(sql, conn);
+                sqlCmd.Parameters.Add(new SqlParameter("@p_projectNumber", projNumber));
+                reader = sqlCmd.ExecuteReader();
+                dataTable = new DataTable();
+                dataTable.Load(reader);
+                reader.Close();
+                dbgvTotal.DataSource = dataTable;
 
-                }
-                catch (SqlException ex)
-                {
-                    MessageBox.Show(ex.Message);
-                }
-                finally
-                {
-                    conn.Close();
-                }
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
             }
         }
 
